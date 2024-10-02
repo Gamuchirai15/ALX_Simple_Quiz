17c1e6d4eb89c80a021ef01cb88fdd4c07beb2bc
Gamuchirai15 <gamiechif@gmail.com>
2024-10-02 20:34:13 +0200

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,47 @@
+function checkAnswer() {
+    const correctAnswer = 4
+    return correctAnswer;
+}
+
+const name = document.querySelector('quiz');
+
+const question = [
+   {
+       question: "What is 2+2?",
+       answers: [
+           { text: "4", correct: true},
+           { text: "22", correct: false},
+           { text: "3", correct: false},
+       ]
+   }
+]
+
+function userAnswer() {
+   const userAnswer = value
+   return userAnswer;
+}
+
+if(correctAnswer === userAnswer) {
+   feedback = ('Correct! Well done.');
+} else {
+   feedback = ("That's incorrect. Try again!");
+}
+
+
+const containerElement = document.getElementById('quiz-container');
+const questionElement = document.getElementById('quiz-question');
+const submitButton = document.getElementById('submit-answer');
+submitButton.addEventListener('click', checkAnswer)
+const feedbackElement = document.getElementById('feedback');
+
+
+function userAnswer(){
+   const submitButton = 4;
+   const isCorrect = submitButton.dataset.correct === "true";
+   if(isCorrect){
+       submitButton.classList.add("Correct! Well done");
+   }else{
+       submitButton.classList.add("That's incorrect. Try again!");
+   }
+}
+
