121d4363aae73d420d7e77d7a2a2af6e581959d7
Gamuchirai15 <gamiechif@gmail.com>
2024-10-03 08:30:58 +0200

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -3,45 +3,4 @@ function checkAnswer() {
     return correctAnswer;
 }
 
-function userAnswer() {
-    const userAnswer = value
-    return userAnswer;
- }
-
-let name = document.querySelector('quiz');
-
-const question = [
-   {
-       question: "What is 2+2?",
-       answers: [
-           { text: "4", correct: true},
-           { text: "22", correct: false},
-           { text: "3", correct: false},
-       ]
-   }
-]
-
-if(correctAnswer === userAnswer) {
-   feedback = ('Correct! Well done.');
-} else {
-   feedback = ("That's incorrect. Try again!");
-}
-
-
-const containerElement = document.getElementById('quiz-container');
-const questionElement = document.getElementById('quiz-question');
-const submitButton = document.getElementById('submit-answer');
-submitButton.addEventListener('click', checkAnswer)
-const feedbackElement = document.getElementById('feedback');
-
-
-function userAnswer(){
-   const submitButton = 4;
-   const isCorrect = submitButton.dataset.correct === "true";
-   if(isCorrect){
-       submitButton.classList.add("Correct! Well done");
-   }else{
-       submitButton.classList.add("That's incorrect. Try again!");
-   }
-}
-
+var userAnswer = value.submitButton;
