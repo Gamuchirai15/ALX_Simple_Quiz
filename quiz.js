6311532ce8a23991f4f15d0c5d1e35605bb3b3f2
Gamuchirai15 <gamiechif@gmail.com>
2024-10-03 10:20:23 +0200

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -5,8 +5,11 @@ function checkAnswer() {
 
 const quiz = document.querySelector("checked");
 
+var userAnswer = value;
+
 if(userAnswer === correctAnswer){
     feedback = ('Correct! Well done.');
 } else {
    feedback = ("That's incorrect. Try again!");
 }
+
